3dec5a232c8a24ffe2af56e9114aaff249e9194b
Dale Williams <dale@dale-williams.com>
2012-01-15 10:30:32 +1100

    Test AutoMapper config is valid

---
+++ Facts/AutoMapper/AutoMappedConfigurationTester.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ElephantQuiz.Web.Infrastructure.AutoMapper;
+using Xunit;
+
+namespace Facts.AutoMapper
+{
+    class AutoMappedConfigurationTester
+    {
+        [Fact]
+        public void AssertConfigurationIsValid()
+        {
+            AutoMapperConfiguration.Configure();
+            Mapper.AssertConfigurationIsValid();
+        }
+    }
+}
